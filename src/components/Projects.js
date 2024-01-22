e8354aeb6f87d19da82350adc0e687c442a4f620
Andy Jung <jungkj@bc.edu>
2024-01-22 10:38:53 -0500

    Updated Projects and worked on Skills element

---
+++ src/components/Projects.js
@@ -0,0 +1,24 @@
+export const Projects = () => {
+    
+    const projects = [
+        {
+            title: "Movie App",
+            description: "",
+            technologies: "Swift, SwiftUI, Google Firebase, MVVM Design",
+            imgURL: projImg1,
+            
+        },
+        {
+            title: "Custom Shell",
+            description: "", 
+            technologies: "C"
+            imgURL: projImg2,
+        },
+        {
+            title: 
+        }
+    ]
+    return (
+
+    )
+}
