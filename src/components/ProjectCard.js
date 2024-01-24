d8a286370e7e05724635fa7e2738b78e6b9d62f5
Andy Jung <jungkj@bc.edu>
2024-01-24 10:13:04 -0500

    Updating comments and code in components

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,17 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, technologies, imgURL, URL}) => {
+    return (
+        <Col sm = {6} md = {4}>
+            <div className = "proj-imgbx">
+                <img src = {imgURL} alt = "Project Img"/>
+                <div className = "proj-txtx">
+                    <h4> {title} </h4>
+                    <h3> Technologies Used: {technologies} </h3>
+                    <span>{description}</span>
+                    <span>{URL} </span>
+                </div>
+            </div>
+        </Col>
+    )
+}
