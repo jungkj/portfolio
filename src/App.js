83d2aab9efd99cbceda22772683bccd00a054482
Andy Jung <jungkj@bc.edu>
2024-03-13 11:20:50 -0400

    Testing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
@@ -11,7 +10,6 @@ import { Footer } from './components/Footer';
 function App() {
   return (
     <div className="App">
-      document.title = "Portfolio";
       <NavBar />
       <Banner />
       <Skills />
